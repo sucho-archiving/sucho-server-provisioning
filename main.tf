52f2557db7cc1aaeaf67d64d1d92a69b4f78fc48
Simon Wiles <simonjwiles@gmail.com>
2022-03-11 16:35:52 -0800

    Add a volume to each droplet on creation
    I've put 100GB in the YAML template.

--- main.tf
+++ main.tf
@@ -15,6 +15,15 @@ locals {
   yaml_data  = [for f in local.yaml_files : yamldecode(file("${path.module}/${f}"))]
 }
 
+resource "digitalocean_volume" "browsertrix" {
+  for_each                = { for o in local.yaml_data : o.droplet_name => o }
+  region                  = var.droplet_region
+  name                    = "${each.value.droplet_name}-volume"
+  size                    = each.value.volume_size
+  initial_filesystem_type = "ext4"
+  description             = "an example volume"
+}
+
 resource "digitalocean_droplet" "browsertrix" {
   for_each   = { for o in local.yaml_data : o.droplet_name => o }
   image      = var.droplet_image
@@ -27,6 +36,12 @@ resource "digitalocean_droplet" "browsertrix" {
   ssh_keys = [
     var.ssh_fingerprint
   ]
+}
+
+resource "digitalocean_volume_attachment" "browsertrix" {
+  for_each   = { for o in local.yaml_data : o.droplet_name => o }
+  droplet_id = digitalocean_droplet.browsertrix[each.key].id
+  volume_id  = digitalocean_volume.browsertrix[each.key].id
 }
 
 output "droplet_ip_address" {
